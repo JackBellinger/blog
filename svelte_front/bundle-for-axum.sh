b99431d65c7ef0f97b3c5c65bb4f8797cd3b8790
jacksonbellinger <5804016+JacksonBellinger@users.noreply.github.com>
2023-09-30 23:21:22 -0400

    Don't change dirs in bundle

--- svelte_front/bundle-for-axum.sh
+++ svelte_front/bundle-for-axum.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mv ./dist/blog/index.html ./dist
-(cd dist/blog/blog && tar c .) | (cd dist && tar xf -)
-rm -rf ./dist/blog
-cp -r ./assets/* ./dist/assets
+#mv ./dist/blog/index.html ./dist
+#(cd dist/blog/blog && tar c .) | (cd dist && tar xf -)
+#rm -rf ./dist/blog
+#cp -r ./assets/* ./dist/assets
 cp ./index.html ./dist/404.html
