f7c3129aa66541ecc00e362ab8f5d11596dd705f
Jack Bellinger <5804016+JackBellinger@users.noreply.github.com>
2023-11-03 21:31:30 -0700

    Add development environment blog (#67)
    * Add development environment blog
    
    * Moved callout component to atoms
    
    * update summary

--- svelte_front/src/lib/components/organisms/Summary.svelte
+++ svelte_front/src/lib/components/organisms/Summary.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section id="about">
-	<div class="info">
+	<div class="info" style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
 		<!-- ### Blogs posts -->
 		<!-- BLOG-POST-LIST:START -->
 		<!-- BLOG-POST-LIST:END -->
@@ -16,98 +16,71 @@
 		<a href="/jackbellinger.github.io/blog/rss" target="blank"><img  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/rss.svg" alt="jackbellinger.github.io/blog/rss" height="30" width="40" /></a>
 		</p>
 		-->
-
-		<div class="langs">
-			<h3>Languages and Tools:</h3>
-			<p>
-				<a href="https://aws.amazon.com" target="_blank" rel="noreferrer">
+		<div class="langs" style="display: flex; align-items: center; flex-direction: row;">
+			<img style="flex: 1 1 50%"
+				src="https://github-readme-stats.vercel.app/api/top-langs?username=jackbellinger&show_icons=true&locale=en&layout=compact&hide=HTML,ShaderLab,ApacheConf,C%23,Shell"
+				alt="most-used-languages"
+			/>
+			<div class="techs" style="display: flex; flex-direction: row; flex-wrap: wrap; flex: 1 1 50%">
+				<a href="https://aws.amazon.com" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
 					<img
 						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
 						alt="aws"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer">
+				<a href="https://azure.microsoft.com/en-in/" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
 					<img
 						src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg"
 						alt="azure"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://cloud.google.com" target="_blank" rel="noreferrer">
-					<img
+				<a href="https://cloud.google.com" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
+					<img id="gcloud-img"
 						src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg"
 						alt="gcp"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://www.java.com" target="_blank" rel="noreferrer">
+				<a href="https://www.java.com" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
 					<img
 						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
 						alt="java"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+				<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
 					<img
 						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
 						alt="javascript"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://www.python.org" target="_blank" rel="noreferrer">
+				<a href="https://www.python.org" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
 					<img
 						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
 						alt="python"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://www.rust-lang.org" target="_blank" rel="noreferrer">
+				<a href="https://www.rust-lang.org" target="_blank" rel="noreferrer" style="height: 5.3831rem; width: 5.3831rem;">
 					<img
 						src="https://raw.githubusercontent.com/devicons/devicon/master/icons/rust/rust-plain.svg"
 						alt="rust"
-						width="40"
-						height="40"
 					/>
 				</a>
-				<a href="https://svelte.dev" target="_blank" rel="noreferrer">
+				<a href="https://svelte.dev" target="_blank" rel="noreferrer" style="height: 5rem; width: 5rem;">
 					<img
 						src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg"
 						alt="svelte"
-						width="40"
-						height="40"
 					/>
 				</a>
-			</p>
-		</div>
-		<p>
-			<a href="https://github.com/ryo-ma/github-profile-trophy"
-				><img src="https://github-profile-trophy.vercel.app/?username=jackbellinger&rank=-?" alt="jackbellinger" /></a
-			>
-		</p>
-		<div class="stats">
-			<p>
-				<img
-					src="https://github-readme-stats.vercel.app/api/top-langs?username=jackbellinger&show_icons=true&locale=en&layout=compact&hide=HTML,ShaderLab,ApacheConf,C%23,Shell"
-					alt="jackbellinger"
-				/>
-			</p>
+			</div>
 
-			<!--
-			<p>&nbsp;<img  src="https://github-readme-stats.vercel.app/api?username=jackbellinger&show_icons=true&locale=en" alt="jackbellinger" /></p>
-			-->
-
-			<p>
-				<img src="https://github-readme-streak-stats.herokuapp.com/?user=jackbellinger&" alt="jackbellinger" />
-			</p>
 		</div>
+
+		<a href="https://github.com/ryo-ma/github-profile-trophy"
+			><img src="https://github-profile-trophy.vercel.app/?username=jackbellinger&rank=-?,-A" alt="jackbellinger" /></a>
+		<a href="https://github.com/Ashutosh00710/github-readme-activity-graph">
+		<img src="https://github-readme-activity-graph.vercel.app/graph?username=JackBellinger&theme=green" alt="activity graph"></a>
+		<p align="left"> <img src="https://komarev.com/ghpvc/?username=jackbellinger&label=Profile%20views&color=0e75b6&style=flat" alt="jackbellinger" /> </p>
 	</div>
+
 </section>
 
 <style lang="scss">
@@ -132,11 +105,11 @@
 			flex-direction: column;
 
 			gap: 10px;
-			h2 {
-				@include for-phone-only {
-					text-align: center;
-				}
-			}
+			//h2 {
+			//	@include for-phone-only {
+			//		text-align: center;
+			//	}
+			//}
 
 			p {
 				@include for-phone-only {
@@ -148,18 +121,27 @@
 				gap: 20px;
 			}
 
-			.langs {
-				display: none;
-				//display: flex;
-				flex-direction: row;
-				gap: 0.5em;
-			}
+			//.langs {
+			//	display: none;
+			//	//display: flex;
+			//	flex-direction: row;
+			//	gap: 0.5em;
+			//}
 
-			.stats {
-				display: flex;
-				flex-direction: row;
-				justify-content: center;
-				gap: 0.5em;
+			//.stats {
+			//	display: flex;
+			//	flex-direction: row;
+			//	justify-content: center;
+			//	gap: 0.5em;
+			//}
+		}
+	}
+
+	.techs {
+		a {
+			img {
+				height: 5.3831em;
+				width: 5.3831em;
 			}
 		}
 	}
